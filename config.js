ce2726783afb36717ddbbf49a29f1840f376dea8
dnsfrbs <76138024+dnsfrbs@users.noreply.github.com>
2025-02-14 07:44:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "wwd.mp3juice.blog%20-%20Jim%20Brickman%2C%20Martina%20McBride%20-%20My%20Valentine%20(Lyrics).mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwxcku2gj/video/upload/v1739489864/qjezuawwxr9k5thg3er0.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
