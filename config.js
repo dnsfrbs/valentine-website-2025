897569a1eb5d5bef7a047dce8021593d87ef623a
dnsfrbs <76138024+dnsfrbs@users.noreply.github.com>
2025-02-14 07:54:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dwxcku2gj/video/upload/v1739489864/qjezuawwxr9k5thg3er0.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dwxcku2gj/video/upload/v1739490766/g81xitygdgqq5kay8hdc.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
